8bc6a93625633a56b7246bd3eac3b50c5b4b4f8b
ganeshlab <52805750+ganeshlab@users.noreply.github.com>
2023-03-14 23:39:26 -0400

    Update dockerbuild.sh

--- deb/dockerbuild.sh
+++ deb/dockerbuild.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 pre=$(curl -sL https://api.github.com/repos/ProtonMail/proton-bridge/releases | jq -r '.[0] | .prerelease')
+getver=$(curl -sL https://api.github.com/repos/ProtonMail/proton-bridge/releases | jq -r '.[0] | .tag_name')
+ver="${getver//v}"
+echo "$ver"-1 > VERSION
 if [ "$pre" = "false" ]; then
- getver=$(curl -sL https://api.github.com/repos/ProtonMail/proton-bridge/releases | jq -r '.[0] | .tag_name')
- ver="${getver//v}"
- echo "$ver"-1 > VERSION
  docker build --no-cache -t ganeshlab/protonbridge:$ver .
 else
- getver=$(curl -sL https://api.github.com/repos/ProtonMail/proton-bridge/releases | jq -r '.[0] | .tag_name')
- ver="${getver//v}"
- echo "$ver"-1 > VERSION
  docker build --no-cache -t ganeshlab/protonbridge:$ver-dev .
 fi
