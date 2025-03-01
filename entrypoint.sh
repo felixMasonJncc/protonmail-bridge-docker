ccd0794ad650cba4819bf0c45c7b068b658c37c3
felixMasonJncc <felix.mason@jncc.gov.uk>
2025-03-01 15:12:59 +0000

    more rational info

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,9 @@ VERSION_INFO="/protonmail/version-info.json"
 # echo bridge_version from version-info.json
 if [[ $1 == version ]]; then
     cat ${VERSION_INFO} | jq -r '.bridge_version'
-    exit 0
-fi
-
-# echo bridge_release_name from version-info.json
-if [[ $1 == release-name ]]; then
     cat ${VERSION_INFO} | jq -r '.bridge_release_name'
     exit 0
 fi
-
 
 # Initialize
 if [[ $1 == init ]]; then
